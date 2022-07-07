0b7e0af6a6e6d1460aaa1d8460c9cf33605c24ef
Anton Undrul <antonundrul@gmail.com>
2022-07-07 18:56:40 +0300

    server request moved to componentDidMount()

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -4,14 +4,13 @@ import axios from "axios";
 
 class Users extends React.Component {
 
-
-
-    constructor(props) {
-        super(props);
+    componentDidMount() {
+        alert('request will be now')
         axios.get('https://social-network.samuraijs.com/api/1.0/users')
             .then(response => {
                 this.props.setUsers(response.data.items)
             });
+        alert('end request')
     }
 
     render() {
